e704c2256f561c530f02a59f7dc12b7bd370cae7
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2023-03-05 19:53:50 +0000

    feat(start-server-script): update env for steam libraries

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/steamcmd/rust/RustDedicated_Data/Plugins/x86_64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/rust/RustDedicated_Data/Plugins/x86_64
 /app/rust/RustDedicated \
     -batchmode \
     -load \
