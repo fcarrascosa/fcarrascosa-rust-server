83344e8334180042a859b24572c7ac8e61fb7cbf
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2023-03-05 04:55:39 +0000

    feat(start-server-script): add path to rust-cmd custom *.so files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/steamcmd/rust/RustDedicated_Data/Plugins:/steamcmd/rust/RustDedicated_Data/Plugins/x86_64
 /app/rust/RustDedicated \
     -batchmode
