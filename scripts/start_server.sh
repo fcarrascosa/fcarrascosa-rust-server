6f65488e2c7cbc52b521fcd2c4a0a226b383527c
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2023-03-07 23:19:17 +0000

    feat(rust-server-image): added more environment variables for rust start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -8,8 +8,12 @@ export LD_LIBRARY_PATH=/app/rust/RustDedicated_Data/Plugins/x86_64
     +server.queryport $SERVER_QUERY_PORT \
     +server.hostname "$SERVER_NAME" \
     +server.description "$SERVER_DESCRIPTION" \
+    +server.level "$SERVER_LEVEL" \
+    +server.worldsize $SERVER_WORLDSIZE \
     +server.url $SERVER_URL \
     +server.identity $SERVER_ID \
+    +server.seed $SERVER_SEED \
+    +server.maxplayers $SERVER_MAXPLAYERS \
     +rcon.port $RCON_PORT \
     +rcon.password $RCON_PASSWORD \
     +rcon.web $RCON_WEB
