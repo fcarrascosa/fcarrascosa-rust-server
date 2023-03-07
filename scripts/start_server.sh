e666c471d9c7680310d29fdd567d92fe5728b1df
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2023-03-07 22:44:22 +0000

    feat(init-script): better processing of env variables

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "LD_LIBRARY_PATH$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH=/app/rust/RustDedicated_Data/Plugins/x86_64
 /app/rust/RustDedicated \
     -batchmode \
@@ -7,8 +6,8 @@ export LD_LIBRARY_PATH=/app/rust/RustDedicated_Data/Plugins/x86_64
     -nographics \
     +server.port $SERVER_PORT \
     +server.queryport $SERVER_QUERY_PORT \
-    +server.hostname $SERVER_NAME \
-    +server.description $SERVER_DESCRIPTION \
+    +server.hostname "$SERVER_NAME" \
+    +server.description "$SERVER_DESCRIPTION" \
     +server.url $SERVER_URL \
     +server.identity $SERVER_ID \
     +rcon.port $RCON_PORT \
