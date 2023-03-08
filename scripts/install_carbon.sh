e4ec86a85fbe4d81ea8e76c2603560dbd42f366f
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2023-03-08 22:34:45 +0000

    feat(install-carbon): fix curl url for carbon

--- scripts/install_carbon.sh
+++ scripts/install_carbon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -c https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz | \
+curl -L "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | \
 tar -xz -C /app/rust/
