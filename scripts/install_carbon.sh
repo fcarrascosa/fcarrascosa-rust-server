53a67cf899e001ac6779330446aece3d2c2664d7
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2023-03-09 09:10:55 +0000

    chore(install-carbon): add download/build info

--- scripts/install_carbon.sh
+++ scripts/install_carbon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CARBON_URL=https://github.com/CarbonCommunity/Carbon.Core/releases/download/${CARBON_VERSION}_build/Carbon.Linux.${CARBON_VERSION == "production" ? "Release": "Debug"}.tar.gz"
 
+echo "Pulling latest Carbon $CARBON_VERSION build from $CARBON_URL"
+
 curl -L "$CARBON_URL" | \
 tar -xz -C /app/rust/
